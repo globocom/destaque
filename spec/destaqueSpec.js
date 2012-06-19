94c22caaaf9b6ba403e54d8ef381797c3c21fc7f
Alexandre Magno <alexanmtz@gmail.com>
2012-06-19 16:45:08 -0300

    making test of methods pause and resume

--- spec/destaqueSpec.js
+++ spec/destaqueSpec.js
@@ -1,51 +1,51 @@
-describe("Destaque Slideshow Plugin", function() {
-  var destaque;
+describe("this.destaque Slideshow Plugin", function() {
 
   beforeEach(function() {
+    jasmine.Clock.useMock();
     loadFixtures("default.html");
     $.fx.off = true;
   });
 
   describe("Plugin initialization", function() {
     beforeEach(function() {
-      destaque = $("#slide-container").destaque({
+      this.destaque = $("#slide-container").destaque({
         itemSelector: ".item",
         itemForegroundElementSelector: ".foreground .element"
       });
     });
 
     it("should use default values", function() {
-      expect(destaque.params.currentSlide).toBe(0);
-      expect(destaque.params.slideSpeed).toBe(1000);
+      expect(this.destaque.params.currentSlide).toBe(0);
+      expect(this.destaque.params.slideSpeed).toBe(1000);
     });
 
     it("should allow the user to override the default values", function() {
-      expect(destaque.params.itemSelector).toBe(".item");
-      expect(destaque.params.itemForegroundElementSelector).toBe(".foreground .element")
+      expect(this.destaque.params.itemSelector).toBe(".item");
+      expect(this.destaque.params.itemForegroundElementSelector).toBe(".foreground .element")
     });
 
     describe("Calculated options", function() {
       it("should mark the component as initialized", function() {
-        expect(destaque.params.initialized).toBe(true);
+        expect(this.destaque.params.initialized).toBe(true);
       });
 
       it("should mark the component as not animating", function() {
-        expect(destaque.params.animating).toBe(false);
+        expect(this.destaque.params.animating).toBe(false);
       });
 
       it("should store the base size", function() {
-        expect(destaque.params.baseSize).toBe( $("#slide-container").width() );
+        expect(this.destaque.params.baseSize).toBe( $("#slide-container").width() );
       });
 
       it("should store the number of slides", function() {
-        expect(destaque.params.slideSum).toBe(4);
+        expect(this.destaque.params.slideSum).toBe(4);
       });
     });
   });
 
   describe("Callbacks", function() {
     beforeEach(function() {
-      destaque = $("#slide-container").destaque({
+      this.destaque = $("#slide-container").destaque({
         itemSelector: ".item",
         itemForegroundElementSelector: ".foreground .element",
 
@@ -59,48 +59,48 @@ describe("Destaque Slideshow Plugin", function() {
     });
 
     it("should call onInit", function() {
-      expect(destaque.params.onInit).toHaveBeenCalledWith(destaque);
+      expect(this.destaque.params.onInit).toHaveBeenCalledWith(this.destaque);
     });
 
     describe("Initial onSlideLoaded callbacks", function() {
       it("should load the first slide", function() {
-        expect(destaque.params.onSlideLoad).
-          toHaveBeenCalledWith(destaque, $(".item.slide-1").get(0), 0);
+        expect(this.destaque.params.onSlideLoad).
+          toHaveBeenCalledWith(this.destaque, $(".item.slide-1").get(0), 0);
       });
 
       it("should load the last slide", function() {
-        expect(destaque.params.onSlideLoad).
-          toHaveBeenCalledWith(destaque, $(".item.slide-4").get(0), 3);
+        expect(this.destaque.params.onSlideLoad).
+          toHaveBeenCalledWith(this.destaque, $(".item.slide-4").get(0), 3);
       });
 
       it("should load the second slide", function() {
-        expect(destaque.params.onSlideLoad).
-          toHaveBeenCalledWith(destaque, $(".item.slide-2").get(0), 1);
+        expect(this.destaque.params.onSlideLoad).
+          toHaveBeenCalledWith(this.destaque, $(".item.slide-2").get(0), 1);
       });
 
       it("should not load the third slide", function() {
-        expect(destaque.params.onSlideLoad).
-          not.toHaveBeenCalledWith(destaque, $(".item.slide-3").get(0), 2);
+        expect(this.destaque.params.onSlideLoad).
+          not.toHaveBeenCalledWith(this.destaque, $(".item.slide-3").get(0), 2);
       });
     });
 
     describe("Automatic slide switching callbacks", function() {
 
       it("should call onResume after resuming", function() {
-        destaque.resume();
-        expect(destaque.params.onResume).toHaveBeenCalledWith(destaque);
+        this.destaque.resume();
+        expect(this.destaque.params.onResume).toHaveBeenCalledWith(this.destaque);
       });
 
       it("should call onPause after pausing", function() {
-        destaque.pause();
-        expect(destaque.params.onPause).toHaveBeenCalledWith(destaque);
+        this.destaque.pause();
+        expect(this.destaque.params.onPause).toHaveBeenCalledWith(this.destaque);
       });
     });
 
     describe("Pagination callbacks", function() {
       beforeEach(function() {
-        destaque.params.currentSlide = 0;
-        destaque.slideSetAndMove("toLeft");
+        this.destaque.params.currentSlide = 0;
+        this.destaque.slideSetAndMove("toLeft");
       });
 
       it("should call beforePageUpdate before paginate", function() {
@@ -109,8 +109,8 @@ describe("Destaque Slideshow Plugin", function() {
           totalSlides: 4
         };
 
-        expect(destaque.params.beforePageUpdate).
-          toHaveBeenCalledWith(destaque, data);
+        expect(this.destaque.params.beforePageUpdate).
+          toHaveBeenCalledWith(this.destaque, data);
       });
 
       it("should call onPageUpdate after paginate", function() {
@@ -119,8 +119,8 @@ describe("Destaque Slideshow Plugin", function() {
           totalSlides: 4
         };
 
-        expect(destaque.params.onPageUpdate).
-          toHaveBeenCalledWith(destaque, data);
+        expect(this.destaque.params.onPageUpdate).
+          toHaveBeenCalledWith(this.destaque, data);
       });
     });
   });
@@ -129,44 +129,44 @@ describe("Destaque Slideshow Plugin", function() {
   describe("Methods", function(){
     beforeEach(function() {
       //jasmine.Clock.useMock();
-      destaque = $("#slide-container").destaque({
+      this.destaque = $("#slide-container").destaque({
         itemSelector: ".item",
         itemForegroundElementSelector: ".foreground .element",
         controlsSelector: '#slide-pagination a'
       });
-      destaque.pause();
+      this.destaque.pause();
     });
     it('should back to first page on restart', function(){
       $("#slide-pagination a[rel='next']").trigger('click');
-      destaque.restart();
-      expect(destaque.params.currentSlide).toBe(0);
+      this.destaque.restart();
+      expect(this.destaque.params.currentSlide).toBe(0);
     });
     afterEach(function(){
-      destaque.restart();
+      this.destaque.restart();
     });
   });
 
   describe("Event listeners", function() {
     beforeEach(function() {
-      destaque = $("#slide-container").destaque({
+      this.destaque = $("#slide-container").destaque({
         itemSelector: ".item",
         itemForegroundElementSelector: ".foreground .element",
         controlsSelector: '#slide-pagination a'
       });
-      destaque.pause();
+      this.destaque.pause();
     });
     describe("Control-based pagination listeners", function() {
       it("should go to next page", function(){
         $("#slide-pagination a[rel='next']").trigger('click');
         expect($('.slide-1').hasClass('active')).toBeFalsy;
         expect($('.slide-2').hasClass('active')).toBeTruthy;
-        expect(destaque.params.currentSlide).toBe(1);
+        expect(this.destaque.params.currentSlide).toBe(1);
       });
       it("should go to previous page", function(){
         $("#slide-pagination a[rel='prev']").trigger('click');
         expect($('.slide-1').hasClass('active')).toBeFalsy;
         expect($('.slide-0').hasClass('active')).toBeTruthy;
-        expect(destaque.params.currentSlide).toBe(3);
+        expect(this.destaque.params.currentSlide).toBe(3);
       });
     });
 
@@ -175,60 +175,94 @@ describe("Destaque Slideshow Plugin", function() {
         var e = jQuery.Event('keydown', {
           keyCode: 39
         });
-        destaque.element.focus();
-        destaque.element.trigger(e);
+        this.destaque.element.focus();
+        this.destaque.element.trigger(e);
         expect($('.slide-1').hasClass('active')).toBeFalsy;
         expect($('.slide-2').hasClass('active')).toBeTruthy;
-        expect(destaque.params.currentSlide).toBe(1);
+        expect(this.destaque.params.currentSlide).toBe(1);
       });
       it("should go to previous page", function(){
         var e = jQuery.Event('keydown', {
           keyCode: 37
         });
-        destaque.element.focus();
-        destaque.element.trigger(e);
+        this.destaque.element.focus();
+        this.destaque.element.trigger(e);
         expect($('.slide-1').hasClass('active')).toBeFalsy;
         expect($('.slide-0').hasClass('active')).toBeTruthy;
-        expect(destaque.params.currentSlide).toBe(3);
+        expect(this.destaque.params.currentSlide).toBe(3);
       });
     });
 
     describe("Gesture-based pagination listeners", function() {
       it("should go to next page", function(){
-        destaque.element.trigger("swipeleft");
+        this.destaque.element.trigger("swipeleft");
         expect($('.slide-1').hasClass('active')).toBeFalsy;
         expect($('.slide-2').hasClass('active')).toBeTruthy;
-        expect(destaque.params.currentSlide).toBe(1);
+        expect(this.destaque.params.currentSlide).toBe(1);
       });
       it("should go to previous page", function(){
-        destaque.element.trigger("swiperight");
+        this.destaque.element.trigger("swiperight");
         expect($('.slide-1').hasClass('active')).toBeFalsy;
         expect($('.slide-0').hasClass('active')).toBeTruthy;
-        expect(destaque.params.currentSlide).toBe(3);
+        expect(this.destaque.params.currentSlide).toBe(3);
       });
     });
 
     describe("Window listeners", function() {
-      // Resize
-    });
-    afterEach(function(){
-      destaque.restart();
+      
     });
   });
 
-  xdescribe("Pause automatic slide switching", function() {
+  describe("Pause automatic slide switching", function() {
+    beforeEach(function() {
+      this.destaque = $("#slide-container").destaque({
+        itemSelector: ".item",
+        itemForegroundElementSelector: ".foreground .element",
+        autoSlideDelay: 0,
+        controlsSelector: '#slide-pagination a'
+      });
+    });
+    it("should pause when method pause is called", function(){
+      expect(this.destaque.params.currentSlide).toBe(0);
+      this.destaque.pause();
+      jasmine.Clock.tick(200);
+      expect(this.destaque.params.currentSlide).toBe(0);
+    });
+    
     it("should only pause if not currently animating", function() {
+      
     });
 
     it("should stop automatic slide switching timer", function() {
+    
     });
   });
 
-  xdescribe("Resume automatic slide switching", function() {
+  describe("Resume automatic slide switching", function() {
+     beforeEach(function() {
+      this.destaque = $("#slide-container").destaque({
+        itemSelector: ".item",
+        itemForegroundElementSelector: ".foreground .element",
+        autoSlideDelay: 200,
+        controlsSelector: '#slide-pagination a'
+      });
+      this.destaque.pause();
+    });
+    it("should resume when resume is called", function(){
+      expect(this.destaque.params.currentSlide).toBe(0);
+      this.destaque.resume();
+      jasmine.Clock.tick(200);
+      expect(this.destaque.params.currentSlide).toBe(1);
+    });
     it("should only resume if not currently animating", function() {
+    
     });
 
     it("should start automatic slide switching timer", function() {
+    
     });
+  });
+  afterEach(function(){
+     this.destaque.restart();
   });
 });
