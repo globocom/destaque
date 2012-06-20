52b89729e69afacfb77b1206fd0372c40f53a647
Alexandre Magno <alexanmtz@gmail.com>
2012-06-20 17:03:47 -0300

    testing with triple slide

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 $(function () {
-  /*$("#slide-container").destaque({
+ $("#slide-container").destaque({
     slideMovement: 100,
     slideSpeed: 1000,
     elementSpeed: 1100,
@@ -7,17 +7,17 @@ $(function () {
     itemSelector: ".item",
     itemForegroundElementSelector: ".foreground .element",
     controlsSelector: "#slide-pagination a"
-  });*/
+  });
   
-  //$('.triple-slides').each(function(){
-    $('.triple-slides:first').destaque({
+  $('.triple-slides').each(function(){
+    $(this).destaque({
       slideMovement: 100,
       slideSpeed: 1000,
       elementSpeed: 1100,
       easingType: "easeInOutExpo",
-      itemSelector: ".item",
-      itemForegroundElementSelector: ".foreground .element"
+      itemSelector: ".item-triple",
+      itemForegroundElementSelector: ".foreground-triple .element"
     });
-  //});
+  });
   
 });
