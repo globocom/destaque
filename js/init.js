0058f5d86539030159664c82886d842754055536
Alexandre Magno <alexanmtz@gmail.com>
2012-06-21 18:22:17 -0300

    making work with slide triple and better documentation with bootstrap e google code prettry

--- js/init.js
+++ js/init.js
@@ -9,15 +9,29 @@ $(function () {
     controlsSelector: "#slide-pagination a"
   });
   
-  $('.triple-slides').each(function(){
-    $(this).destaque({
+  var destaques = [];
+  $('.triple-slides').each(function(index, el){
+    destaques[index] = $(this).destaque({
       slideMovement: 100,
       slideSpeed: 1000,
       elementSpeed: 1100,
+      stopOnMouseOver: false,
       easingType: "easeInOutExpo",
       itemSelector: ".item-triple",
-      itemForegroundElementSelector: ".foreground-triple .element"
+      itemForegroundElementSelector: ".foreground-triple .element",
+      controlsSelector: "#slide-triple-pagination a"
     });
+   
   });
   
+ $('.triple-slides').mouseover(function(){
+    for(var i=0; i<destaques.length; i++) {
+      destaques[i].pause();
+    }    
+  }).mouseleave(function() {
+    for(var i=0; i<destaques.length; i++) {
+      destaques[i].resume();
+    }
+  });
+
 });
