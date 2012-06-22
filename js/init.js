a70c4bdc185cd48abbad80ef786010813bc618a6
Alexandre Magno <alexanmtz@gmail.com>
2012-06-22 15:42:11 -0300

    making the triple destaque work with a delay, stil have to make it work with click handlers

--- js/init.js
+++ js/init.js
@@ -1,3 +1,23 @@
+var destaques = [];
+function callingDestaqueTriple(index, el) {
+  destaques[index] = $(el).destaque({
+    slideMovement: 100,
+    slideSpeed: 1000,
+    autoSlideDelay: 3000,
+    elementSpeed: 1100,
+    stopOnMouseOver: false,
+    easingType: "easeInOutExpo",
+    itemSelector: ".item-triple",
+    itemForegroundElementSelector: ".foreground-triple .element",
+    controlsSelector: "#slide-triple-pagination a"
+  });
+}
+
+function resumeDestaqueTriple(i) {
+  console.debug(i, destaques[0]);
+  destaques[i].resume();
+}
+
 $(function () {
  $("#slide-container").destaque({
     slideMovement: 100,
@@ -8,31 +28,21 @@ $(function () {
     itemForegroundElementSelector: ".foreground .element",
     controlsSelector: "#slide-pagination a"
   });
-  
-  var destaques = [];
+ 
   $('.triple-slides').each(function(index, el){
-    destaques[index] = $(this).destaque({
-      slideMovement: 100,
-      slideSpeed: 1000,
-      autoSlideDelay: 3000,
-      elementSpeed: 1100,
-      stopOnMouseOver: false,
-      easingType: "easeInOutExpo",
-      itemSelector: ".item-triple",
-      itemForegroundElementSelector: ".foreground-triple .element",
-      controlsSelector: "#slide-triple-pagination a"
-    });
-   
-  });
+    window.setTimeout(function(){
+      callingDestaqueTriple(index, el);
+    }, index*250);
+  }); 
   
  $('.triple-slides').mouseover(function(){
-    for(var i=0; i<destaques.length; i++) {
-      destaques[i].pause();
-    }    
-  }).mouseleave(function() {
-    for(var i=0; i<destaques.length; i++) {
-      destaques[i].resume();
-    }
-  });
+   for(var i=0; i<destaques.length; i++) {
+     destaques[i].pause();
+   }    
+ }).mouseleave(function() {
+  for(var i=0; i<destaques.length; i++) {
+     window.setTimeout(resumeDestaqueTriple, i*250, i);
+  }
+ });
 
 });
