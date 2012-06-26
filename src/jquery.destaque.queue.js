5bcb054a4fa1b528790afd404b928ebbba4921db
tulios <tulio@corp.globo.com>
2012-06-26 16:13:09 -0300

    adding onInit callback and refatoring the code to keep the same pattern as jquery.destaque

--- src/jquery.destaque.queue.js
+++ src/jquery.destaque.queue.js
@@ -13,90 +13,119 @@
  *   jquery.easing
  */
 
-;(function ( $, window, document, undefined ) {
+;(function ($, window, document, undefined) {
 
-    $.fn.destaquesQueue = function ( options ) {
+  $.fn.destaquesQueue = function (options) {
+    var options = $.extend({}, $.fn.destaque.options, options);
+    return new DestaqueWrapper(options, $(this));
+  }
 
-        var options = $.extend( {}, $.fn.destaquesQueue.options, options );
-        var instances = [];
-        var currentSlide = 1;
-        var Destaques = {
-          init: function(index, el) {
-              window.setTimeout(function(){
-                instances[index] = $(el).destaque({
-                slideMovement: options.slideMovement,
-                slideSpeed: options.slideSpeed,
-                autoSlideDelay: options.SlideDelay,
-                elementSpeed: options.elementSpeed,
-                stopOnMouseOver: false,
-                easingType: options.easingType,
-                itemSelector: options.itemSelector,
-                itemForegroundElementSelector: options.itemForegroundElementSelector,
-                onPageUpdate: function(slideshow, data) {
-                  if(currentSlide !== data.currentSlide){
-                     options.onPageUpdate(this, slideshow, data);
-                  }
-                  currentSlide = data.currentSlide;
-                }
-              });  
-            }, index*options.delay);
-            
-            
-            //instances[index].pause();
-          },
-          pause: function() {
-            for(var i=0; i<instances.length; i++) {
-             instances[i].pause();
-            }     
-          },
-          move: function(i, direction){
-            window.setTimeout(function(){
-              instances[i].slideSetAndMove(direction);
-            }, i*options.delay);
-          },
-          resume: function(i) {
-            window.setTimeout(function(){
-              instances[i].resume();
-            }, i*options.delay)
-             
+  var DestaqueWrapper = function(options, elements) {
+    this.options = options;
+    this.elements = elements;
+    this.instances = [];
+    this.currentSlide = 1;
+
+    this._initializeMouseEvents();
+    this._initializeControls();
+    var self = this;
+
+    for(var i = 0; i < this.elements.length; i++) {
+      this._initializeDestaque(i);
+    }
+
+    this.options.onInit(this);
+  }
+
+  DestaqueWrapper.prototype = {
+    _initializeDestaque: function(index) {
+      var self = this;
+      window.setTimeout(function(){
+        var element = self.elements.get(index);
+        self.instances[index] = $(element).destaque({
+          slideMovement: self.options.slideMovement,
+          slideSpeed: self.options.slideSpeed,
+          autoSlideDelay: self.options.autoSlideDelay,
+          elementSpeed: self.options.elementSpeed,
+          stopOnMouseOver: false,
+          easingType: self.options.easingType,
+          itemSelector: self.options.itemSelector,
+          itemForegroundElementSelector: self.options.itemForegroundElementSelector,
+          onPageUpdate: function(slideshow, pageData) {
+            if(self.currentSlide !== pageData.currentSlide){
+              self.options.onPageUpdate(self, slideshow, pageData);
+            }
+            self.currentSlide = pageData.currentSlide;
           }
-        };
-        if(options.stopOnMouseOver) {
-          this.parent().mouseover(function(){
-            Destaques.pause();            
-           }).mouseleave(function() {
-              for(var i=0; i<instances.length; i++) {
-                 Destaques.resume(i);
-              }
-           });          
+        });
+
+      }, index * self.options.delay);
+    },
+
+    _initializeMouseEvents: function() {
+      if (this.options.stopOnMouseOver) {
+
+        var self = this;
+        var parent = $(this.elements).parent();
+        parent.mouseover(function(){
+          self.pause();
+        });
+
+        parent.mouseleave(function() {
+          for(var i = 0; i < self.instances.length; i++) {
+            self.resume(i);
+          }
+        });
+
+      }
+    },
+
+    _initializeControls: function() {
+      var self = this;
+      $(this.options.controlsSelector).bind('click', function(e) {
+        e.preventDefault();
+
+        var direction = $(this).attr("rel") === "prev" ? "toRight" : "toLeft";
+        for(var i = 0; i < self.instances.length; i++) {
+          self.move(i, direction);
         }
-        $(options.controlsSelector).bind('click', function() {
-          var direction = $(this).attr("rel") === "prev" ? "toRight" : "toLeft";
-          for(var i=0; i<instances.length; i++) {
-            Destaques.move(i, direction);
-          }
-          return false;
-        });
-        return this.each(function (index, el) {
-              var elem = $(this);
-              Destaques.init(index,el);
-        });
-    };
+      });
+    },
 
-    // Globally overriding options
+    pause: function() {
+      for(var i = 0; i < this.instances.length; i++) {
+        this.instances[i].pause();
+      }
+    },
 
-    $.fn.destaquesQueue.options = {
-        delay: 250,
-        controlsSelector: "#slide-triple-pagination a",
-        stopOnMouseOver: true,
-        slideMovement: 100,
-        slideSpeed: 1000,
-        autoSlideDelay: 3000,
-        elementSpeed: 1100,
-        easingType: "easeInOutExpo",
-        itemSelector: ".item-triple",
-        itemForegroundElementSelector: ".foreground-triple .element",
-        onPageUpdate: function(slideshow, data){}
-    };
+    move: function(index, direction){
+      var self = this;
+      window.setTimeout(function(){
+        self.instances[index].slideSetAndMove(direction);
+      }, index * this.options.delay);
+    },
 
-})( jQuery, window, document );
+    resume: function(index) {
+      var self = this;
+      window.setTimeout(function(){
+        self.instances[index].resume();
+      }, index * this.options.delay)
+    }
+  }
+
+  $.fn.destaquesQueue.options = {
+    delay: 250,
+    stopOnMouseOver: true,
+    controlsSelector: "#slide-triple-pagination a",
+    slideMovement: 100,
+    slideSpeed: 1000,
+    autoSlideDelay: 3000,
+    elementSpeed: 1100,
+    easingType: "easeInOutExpo",
+    itemSelector: ".item-triple",
+    itemForegroundElementSelector: ".foreground-triple .element",
+    onPageUpdate: function(slideshow, pageData){},
+    onInit: function() {}
+  };
+
+})(jQuery, window, document, undefined);
