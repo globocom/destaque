a0c3515c1f036fcc2486ab460dcbd779d9db7af0
Alexandre Magno <alexanmtz@gmail.com>
2012-07-04 19:15:51 -0300

    updating destaqueQueue to work with request animation frame and fallback for ie

--- src/jquery.destaque.queue.js
+++ src/jquery.destaque.queue.js
@@ -1,16 +1,7 @@
 /*!
- *
- * @name jQuery destaque queue
- * @namespace jQuery
- * @author Alexandre Magno, Túlio Ornelas, Daniel Fernandes and Emerson Macedo (http://blog.alexandremagno.net)
- * @version 0.1
- * @description Use this component to make destaque work enhanced for more than one destaque in a page
- * @requires
- *   jquery.destaque
- *   jquery.ui.core.js
- *   jquery.ui.widget.js
- *   jquery.scrollTo
- *   jquery.easing
+ * jQuery Destaque Queue - Make jquery.destaque work enhanced for more than one instance in a page
+ * https://github.com/globocom/destaque
+ * version: 0.1.0
  */
 
 ;(function ($, window, document, undefined) {
@@ -18,23 +9,79 @@
   $.fn.destaquesQueue = function (options) {
     var options = $.extend({}, $.fn.destaquesQueue.options, options);
     return new DestaqueWrapper(options, $(this));
-  }
+  };
+
+  $.fn.destaquesQueue.options = {
+    delay: 250,
+    stopOnMouseOver: true,
+    controlsSelector: "#slide-triple-pagination a",
+    slideMovement: 100,
+    slideSpeed: 1000,
+    autoSlideDelay: 3000,
+    elementSpeed: 1100,
+    easingType: "easeInOutExpo",
+    itemSelector: ".item-triple",
+    itemForegroundElementSelector: ".foreground-triple .element",
+    onPageUpdate: function(slideshow, pageData){},
+    onInit: function() {}
+  };
 
   var DestaqueWrapper = function(options, elements) {
     this.options = options;
     this.elements = elements;
     this.instances = [];
     this.currentSlide = 0;
+    this.delayTime = 0;
 
     this._initializeMouseEvents();
     this._initializeControls();
     this._initKeyboardListeners();
     this._queue(this._initializeDestaque);
-    
+
     this.options.onInit.call(this);
-  }
+  };
 
   DestaqueWrapper.prototype = {
+    pause: function() {
+      this._queue(this.pauseFor);
+    },
+
+    pauseFor: function(index) {
+      this.instances[index].pause();
+    },
+
+    resume: function() {
+      this._queue(this.resumeFor);
+    },
+
+    resumeFor: function(index) {
+      this.instances[index].resume();
+    },
+
+    slideSetAndMove: function(direction) {
+      this._queue(this.slideSetAndMoveFor, direction);
+    },
+
+    slideSetAndMoveFor: function(index, direction) {
+      this.instances[index].slideSetAndMove(direction);
+    },
+
+    goTo: function(slideNumber) {
+      this._queue(this.goToFor, slideNumber);
+    },
+
+    goToFor: function(index, slideNumber) {
+      this.instances[index].goTo(slideNumber);
+    },
+
+    refresh: function(newParams) {
+      this._queue(this.refreshFor, newParams);
+    },
+
+    refreshFor: function(index, newParams) {
+      this.instances[index].refresh(newParams);
+    },
+
     _initializeDestaque: function(index) {
       var self = this;
       var element = self.elements.get(index);
@@ -47,29 +94,51 @@
         easingType: self.options.easingType,
         itemSelector: self.options.itemSelector,
         itemForegroundElementSelector: self.options.itemForegroundElementSelector,
+
         onPageUpdate: function(slideshow, pageData) {
           if(self.currentSlide !== pageData.currentSlide){
             self.options.onPageUpdate(self, slideshow, pageData);
           }
+
           self.currentSlide = pageData.currentSlide;
-          
         },
+
         onInit: function() {
           $("body").unbind('keydown.destaque');
         }
       });
     },
-    
+
     _queue: function(method) {
       var self = this;
       var args = arguments[1];
-      $(self.elements).each(function(index, el){
-        window.setTimeout(function(){
-          method.call(self, index, args);
-        }, self.options.delay * index);        
-      });
+      var start = window.mozAnimationStartTime || new Date().getTime();  
+      var index = 0;
+      window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       || 
+              window.webkitRequestAnimationFrame || 
+              window.mozRequestAnimationFrame    || 
+              window.oRequestAnimationFrame      || 
+              window.msRequestAnimationFrame     || 
+              function( callback ){
+                window.setTimeout(callback, self.options.delay*index);
+              };
+      })();
+     
+      function step(timestamp) {  
+        var progress = timestamp - start;  
+        if (progress < self.options.delay*index) {  
+          requestAnimFrame(step);  
+        } else {
+          if(index < self.elements.length) {
+            method.call(self, index, args);
+            requestAnimFrame(step);
+            index++;            
+          }
+        }  
+      }  
+      requestAnimFrame(step);  
     },
-
     _initializeMouseEvents: function() {
       if (this.options.stopOnMouseOver) {
 
@@ -93,48 +162,22 @@
         e.preventDefault();
 
         var direction = $(this).attr("rel") === "prev" ? "toRight" : "toLeft";
-        self._queue(self.move, direction);
+        self._queue(self.slideSetAndMoveFor, direction);
       });
     },
-    
+
     _initKeyboardListeners: function() {
       var self = this;
       $("body").bind("keydown.destaqueQueue", function(e) {
-        
+
         if (e.keyCode === 37) {
-          self._queue(self.move, "toRight");
+          self._queue(self.slideSetAndMoveFor, "toRight");
         } else {
-          self._queue(self.move, "toLeft");
+          self._queue(self.slideSetAndMoveFor, "toLeft");
         }
+
       });
-    },
-
-    pause: function(index) {
-      this.instances[index].pause();
-    },
-
-    resume: function(index) {
-      this.instances[index].resume();
-    },
-
-    move: function(index, direction){
-      this.instances[index].slideSetAndMove(direction);
     }
-  }
-
-  $.fn.destaquesQueue.options = {
-    delay: 250,
-    stopOnMouseOver: true,
-    controlsSelector: "#slide-triple-pagination a",
-    slideMovement: 100,
-    slideSpeed: 1000,
-    autoSlideDelay: 3000,
-    elementSpeed: 1100,
-    easingType: "easeInOutExpo",
-    itemSelector: ".item-triple",
-    itemForegroundElementSelector: ".foreground-triple .element",
-    onPageUpdate: function(slideshow, pageData){},
-    onInit: function() {}
   };
 
 })(jQuery, window, document, undefined);
